f602d1c26fccbf485d2fa222382be15865c5c68a
mahad446 <mahadmubashir43@gmail.com>
2025-04-10 15:07:06 -0700

    activity32

---
+++ activity#2.py
@@ -0,0 +1,8 @@
+setx = {"green" , "blue"}
+sety = {"blue , yellow"}
+print("orniginal set elements")
+print(setx)
+print(sety)
+print("/ntntersection of two said sets:")
+setz =setx.intersection(sety)
+print(setz)
